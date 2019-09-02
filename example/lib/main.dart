16a9193ccb25e2443b886ca6531410ce581906db
蔡凯歌 <caikaige.g@gmail.com>
2019-09-02 21:51:12 +0800

    rebuild

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
 
@@ -25,19 +24,14 @@ class _MyAppState extends State<MyApp> {
     await DemoPlugin.showToast('然后我们来测试');
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       platformVersion = await DemoPlugin.platformVersion;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
