d609ad90286bf0df9a1e5028dc0eb1b38d382ddf
蔡凯歌 <caikaige.g@gmail.com>
2019-09-03 16:33:33 +0800

    rebuild

--- ios/Classes/SwiftDemoPlugin.swift
+++ ios/Classes/SwiftDemoPlugin.swift
@@ -2,18 +2,18 @@ import Flutter
 import UIKit
 
 public class SwiftDemoPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "demo_plugin", binaryMessenger: registrar.messenger())
-    let instance = SwiftDemoPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if (call.method.elementsEqual("nativeToast")) {
-        let args = call.arguments as? NSDictionary
-        let msg = args["msg"] as? String
-        UIAlertView(title:"", message:msg, delegate:nil, cancelButtonTitle:"OK")
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "demo_plugin", binaryMessenger: registrar.messenger())
+        let instance = SwiftDemoPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if (call.method.elementsEqual("nativeToast")) {
+            let args = call.arguments as? NSDictionary
+            let msg = args!["msg"] as? String
+            UIAlertView(title:"", message:msg, delegate:nil, cancelButtonTitle:"OK")
+        }
+        result("iOS " + UIDevice.current.systemVersion) 
+    }
 }
--- lib/demo_plugin.dart
+++ lib/demo_plugin.dart
@@ -15,3 +15,4 @@ class DemoPlugin {
     await _channel.invokeMethod('nativeToast', args);
   }
 }
+ 
